c39372d61a81a8a67a36226081acc6c8d86d5473
Answer Tjiane <answer.tjiane@eoh.com>
2020-03-07 19:56:14 +0200

    07

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import SwitchNavigator from './navigation/SwitchNavigator.js'
-import { Provider } from 'react-redux'
-import firebase from './config/firebase'
+import SwitchNavigator from './navigation/SwitchNavigator';
+import { Provider } from 'react-redux';
+import firebase from './config/firebase';
 import store from './store';
 console.disableYellowBox = true;
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CravEat-V2
-The advanced development of crav-eat using expo, firebase and redux
+The advanced development of crav-eat using expo, firebase and redux     
+
+#TO BUILD ANDROID APP BUNDLE[AAB] https://docs.expo.io/versions/latest/distribution/building-standalone-apps/
