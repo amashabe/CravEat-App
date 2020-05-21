f52e3b57e7c12f586f4de6319e515c2794b1f9ad
Arafat Mashabela <30957613+amashabe@users.noreply.github.com>
2020-05-21 21:09:06 +0200

    cxomments

--- actions/post.js
+++ actions/post.js
@@ -1,173 +1,208 @@
-import firebase from 'firebase';
-import db from '../config/firebase';
-import { UPDATE_DESCRIPTION, GET_POSTS, UPDATE_RECIPE, UPDATE_PHOTO, UPDATE_LOCATION, GET_COMMENTS, GET_POST, GET_USER_POSTS } from '../types';
-import cloneDeep from 'lodash/cloneDeep'
-import orderBy from 'lodash/orderBy';
+import firebase from "firebase";
+import db from "../config/firebase";
+import {
+  UPDATE_DESCRIPTION,
+  GET_POSTS,
+  UPDATE_RECIPE,
+  UPDATE_PHOTO,
+  UPDATE_LOCATION,
+  GET_COMMENTS,
+  GET_POST,
+  GET_USER_POSTS,
+} from "../types";
+import cloneDeep from "lodash/cloneDeep";
+import orderBy from "lodash/orderBy";
 
 export const clearPhoto = () => {
-	return { type: UPDATE_PHOTO, payload: '' }
-}
+  return { type: UPDATE_PHOTO, payload: "" };
+};
 
 export const updateDescription = (text) => {
-	return { type: UPDATE_DESCRIPTION, payload: text }
-}
+  return { type: UPDATE_DESCRIPTION, payload: text };
+};
 
 export const updateRecipe = (text) => {
-	return { type: UPDATE_RECIPE, payload: text }
-}
+  return { type: UPDATE_RECIPE, payload: text };
+};
 
 export const updatePhoto = (text) => {
-	return { type: UPDATE_PHOTO, payload: text }
-}
+  return { type: UPDATE_PHOTO, payload: text };
+};
 
 export const updateLocation = (text) => {
-	return { type: UPDATE_LOCATION, payload: text }
-}
+  return { type: UPDATE_LOCATION, payload: text };
+};
 
 export const getPost = (postId, navigate) => async (dispatch) => {
-	const post = await db.collection('posts').doc(postId).get()
-	dispatch({ type: GET_POST, payload: post.data() })
-	navigate.navigate('Comment', post.data())
-}
+  const post = await db.collection("posts").doc(postId).get();
+  dispatch({ type: GET_POST, payload: post.data() });
+  navigate.navigate("Comment", post.data());
+};
 
 export const uploadPost = () => async (dispatch, getState) => {
-	const { post, user } = getState();
-	let newPosts = cloneDeep(post.feed.reverse())
-	try {
-		const upload = {
-			postPhoto: post.photo,
-			postDescription: post.description,
-			uid: user.uid,
-			photo: user.photo,
-			username: user.username,
-			postRecipe: post.recipe,
-			postLocation: post.location,
-			createdAt: new Date().getTime(),
-			likes: [],
-			comments: [],
-			email: user.email
-		}
-		const ref = await db.collection('posts').doc()
-		upload.id = ref.id
-		ref.set(upload)
-		newPosts.push(upload)
-		dispatch({ type: UPDATE_PHOTO, payload: '' })
-		dispatch({ type: UPDATE_LOCATION, payload: '' })
-		dispatch({ type: UPDATE_DESCRIPTION, payload: '' })
-		dispatch({ type: UPDATE_RECIPE, payload: '' })
-	} catch (e) {
-		alert(e)
-	}
-}
+  const { post, user } = getState();
+  let newPosts = cloneDeep(post.feed.reverse());
+  try {
+    const upload = {
+      postPhoto: post.photo,
+      postDescription: post.description,
+      uid: user.uid,
+      photo: user.photo,
+      username: user.username,
+      postRecipe: post.recipe,
+      postLocation: post.location,
+      createdAt: new Date().getTime(),
+      likes: [],
+      comments: [],
+      email: user.email,
+    };
+    const ref = await db.collection("posts").doc();
+    upload.id = ref.id;
+    ref.set(upload);
+    newPosts.push(upload);
+    dispatch({ type: UPDATE_PHOTO, payload: "" });
+    dispatch({ type: UPDATE_LOCATION, payload: "" });
+    dispatch({ type: UPDATE_DESCRIPTION, payload: "" });
+    dispatch({ type: UPDATE_RECIPE, payload: "" });
+  } catch (e) {
+    alert(e);
+  }
+};
 
 export const getPosts = () => (dispatch, getState) => {
-	try {
-		db.collection('posts').onSnapshot((querySnapshot) => {
-			let array = []
-			querySnapshot.forEach((post) => {
-				array.push(post.data())
-			})
-			dispatch({ type: GET_POSTS, payload: orderBy(array, 'createdAt', 'desc') })
-		})
-	} catch (e) {
-		alert(e)
-	}
-}
+  try {
+    db.collection("posts").onSnapshot((querySnapshot) => {
+      let array = [];
+      querySnapshot.forEach((post) => {
+        array.push(post.data());
+      });
+      dispatch({
+        type: GET_POSTS,
+        payload: orderBy(array, "createdAt", "desc"),
+      });
+    });
+  } catch (e) {
+    alert(e);
+  }
+};
 
 export const getUserPosts = () => async (dispatch, getState) => {
-	try {
-		const { user } = getState();
-		const posts = await db.collection('posts').get()
+  try {
+    const { user } = getState();
+    const posts = await db.collection("posts").get();
 
-		let array = []
-		posts.forEach((post) => {
-			if (user.uid === post.data().uid) {
-				array.push(post.data())
-			}
-		})
-		dispatch({ type: GET_USER_POSTS, payload: orderBy(array, 'createdAt', 'desc') })
-	} catch (e) {
-		alert(e)
-	}
-}
+    let array = [];
+    posts.forEach((post) => {
+      if (user.uid === post.data().uid) {
+        array.push(post.data());
+      }
+    });
+    dispatch({
+      type: GET_USER_POSTS,
+      payload: orderBy(array, "createdAt", "desc"),
+    });
+  } catch (e) {
+    alert(e);
+  }
+};
 
 export const likePost = (post) => (dispatch, getState) => {
-	const { uid, username, photo } = getState().user
-	try {
-		db.collection('posts').doc(post.id).update({
-			likes: firebase.firestore.FieldValue.arrayUnion(uid)
-		})
-		if (uid !== post.uid) {
-			db.collection('notifications').doc().set({
-				postId: post.id,
-				postPhoto: post.postPhoto,
-				likerId: uid,
-				likerPhoto: photo,
-				likerName: username,
-				uid: post.uid,
-				createdAt: new Date().getTime(),
-				type: 'LIKE',
-				read: false
-			})
-		}
-		dispatch(getPosts())
-	} catch (e) {
-		alert(e)
-	}
-}
+  const { uid, username, photo } = getState().user;
+  try {
+    db.collection("posts")
+      .doc(post.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayUnion(uid),
+      });
+    if (uid !== post.uid) {
+      db.collection("notifications").doc().set({
+        postId: post.id,
+        postPhoto: post.postPhoto,
+        likerId: uid,
+        likerPhoto: photo,
+        likerName: username,
+        uid: post.uid,
+        createdAt: new Date().getTime(),
+        type: "LIKE",
+        read: false,
+      });
+    }
+    dispatch(getPosts());
+  } catch (e) {
+    alert(e);
+  }
+};
 
 export const unlikePost = (post) => async (dispatch, getState) => {
-	const { uid } = getState().user;
-	try {
-		db.collection('posts').doc(post.id).update({
-			likes: firebase.firestore.FieldValue.arrayRemove(uid)
-		})
-		const query = await db.collection('notifications').where('postId', '==', post.id).where('likerId', '==', uid).get();
-		query.forEach((response) => {
-			response.ref.delete()
-		})
-		dispatch(getPosts())
-	} catch (e) {
-		console.log(e)
-	}
-}
+  const { uid } = getState().user;
+  try {
+    db.collection("posts")
+      .doc(post.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(uid),
+      });
+    const query = await db
+      .collection("notifications")
+      .where("postId", "==", post.id)
+      .where("likerId", "==", uid)
+      .get();
+    query.forEach((response) => {
+      response.ref.delete();
+    });
+    dispatch(getPosts());
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-export const getComments = (post) => dispatch => {
-	try {
-		db.collection('posts').doc(post.id).onSnapshot((querySnapshot) => {
-			dispatch({ type: GET_COMMENTS, payload: orderBy(querySnapshot.data().comments, 'createdAt', 'desc') })
-		})
-	} catch (error) {
-		console.log(error)
-	}
-
-}
+export const getComments = (post) => (dispatch) => {
+  try {
+    db.collection("comments").onSnapshot((querySnapshot) => {
+      const array = [];
+      querySnapshot.forEach((doc) => {
+          array.push(doc.data());
+      });
+      dispatch({
+        type: GET_COMMENTS,
+        payload: orderBy(array, "createdAt", "desc"),
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const addComment = (text, post) => async (dispatch, getState) => {
-	const { uid, photo, username } = getState().user
-	let newComments = cloneDeep(getState().post.comments.reverse())
-	try {
-		const comment = {
-			comment: text,
-			commenterId: uid,
-			commenterPhoto: photo,
-			commenterName: username,
-			createdAt: new Date().getTime(),
-		}
-		db.collection('posts').doc(post.id).update({
-			comments: firebase.firestore.FieldValue.arrayUnion(comment)
-		})
-		comment.postId = post.id
-		comment.postPhoto = post.postPhoto
-		comment.uid = post.uid
-		comment.type = 'COMMENT'
-		comment.read = false
-		newComments.push(comment)
+  const { uid, photo, username } = getState().user;
+  let newComments = cloneDeep(getState().post.comments.reverse());
+  try {
+    const comment = {
+      comment: text,
+      commenterId: uid,
+      commenterPhoto: photo,
+      commenterName: username,
+      createdAt: new Date().getTime(),
+    };
+    const ref = await db.collection("comments").doc();
+    comment.postId = post.id; 
+    comment.id = ref.id;
+    ref.set(comment);
+    db.collection("posts")
+      .doc(post.id)
+      .update({
+        comments: firebase.firestore.FieldValue.arrayUnion(comment),
+      });
+    comment.postId = post.id;
+    comment.postPhoto = post.postPhoto;
+    comment.uid = post.uid;
+    comment.type = "COMMENT";
+    comment.read = false;
+    newComments.push(comment);
 
-		if (uid !== post.uid) {
-			db.collection('notifications').doc().set(comment)
-		}
-	} catch (e) {
-		console.log(e)
-	}
-}
+    if (uid !== post.uid) {
+      db.collection("notifications").doc().set(comment);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
--- actions/user.js
+++ actions/user.js
@@ -1,278 +1,325 @@
-import firebase from 'firebase';
-import { Keyboard } from 'react-native';
-import db from '../config/firebase';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
-import md5 from 'react-native-md5';
-import { getPosts } from './post';
-import { UPDATE_EMAIL, UPDATE_PASSWORD, UPDATE_USERNAME, UPDATE_BIO, SIGN_IN, SIGN_OUT, LOADING, SET_ERROR, SET_TOKEN, GET_ALL_USERS, UPDATE_PROFILE_PICTURE, GET_PROFILE } from '../types';
+import firebase from "firebase";
+import { Keyboard } from "react-native";
+import db from "../config/firebase";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import md5 from "react-native-md5";
+import { getPosts } from "./post";
+import {
+  UPDATE_EMAIL,
+  UPDATE_PASSWORD,
+  UPDATE_USERNAME,
+  UPDATE_BIO,
+  SIGN_IN,
+  SIGN_OUT,
+  LOADING,
+  SET_ERROR,
+  SET_TOKEN,
+  GET_ALL_USERS,
+  UPDATE_PROFILE_PICTURE,
+  GET_PROFILE,
+} from "../types";
 
 export const updateEmail = (email) => {
-	return {
-		type: UPDATE_EMAIL, payload: email
-	}
-}
+  return {
+    type: UPDATE_EMAIL,
+    payload: email,
+  };
+};
 
-export const updateToken = text => {
-	return { type: SET_TOKEN, payload: text }
-}
+export const updateToken = (text) => {
+  return { type: SET_TOKEN, payload: text };
+};
 
 export const updatePassword = (password) => {
-	return {
-		type: UPDATE_PASSWORD, payload: password,
-	}
-}
+  return {
+    type: UPDATE_PASSWORD,
+    payload: password,
+  };
+};
 
 export const updateUsername = (username) => {
-	return {
-		type: UPDATE_USERNAME, payload: username,
-	}
-}
+  return {
+    type: UPDATE_USERNAME,
+    payload: username,
+  };
+};
 
 export const updateBio = (bio) => {
-	return {
-		type: UPDATE_BIO, payload: bio,
-	}
-}
+  return {
+    type: UPDATE_BIO,
+    payload: bio,
+  };
+};
 
 export const updatePP = () => async (dispatch, getState) => {
-	const { uid } = getState().user
-	if (Constants.platform.ios) {
-		const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-		if (status !== 'granted') {
-			alert('Sorry, we need camera roll permissions to make this work!');
-		}
-	}
+  const { uid } = getState().user;
+  if (Constants.platform.ios) {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== "granted") {
+      alert("Sorry, we need camera roll permissions to make this work!");
+    }
+  }
 
-	let result = await ImagePicker.launchImageLibraryAsync({
-		mediaTypes: ImagePicker.MediaTypeOptions.Images,
-		allowsEditing: true,
-		aspect: [4, 3],
-	});
-	if (!result.cancelled) {
-		dispatch({ type: LOADING, payload: true })
-		const respond = await fetch(result.uri);
-		const file = await respond.blob();
-		const storageRef = firebase.storage().ref();
-		const metadata = { contentType: 'image/jpeg' };
-		let uploadTask = storageRef.child('ProfilePicture/' + uid).put(file, metadata);
-		uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-			(snapshot) => {
-				let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				if (progress === 0) {
-
-				}
-				if (progress === 100) {
-
-				}
-
-			}, (error) => {
-				alert(error)
-			}, () => {
-				uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-					db.collection('users').doc(uid).update({ photo: downloadURL })
-					dispatch(batchUpdate(downloadURL, uid))
-					dispatch({ type: LOADING, payload: false })
-				}
-				);
-			});
-	}
-}
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [4, 3],
+  });
+  if (!result.cancelled) {
+    dispatch({ type: LOADING, payload: true });
+    const respond = await fetch(result.uri);
+    const file = await respond.blob();
+    const storageRef = firebase.storage().ref();
+    const metadata = { contentType: "image/jpeg" };
+    let uploadTask = storageRef
+      .child("ProfilePicture/" + uid)
+      .put(file, metadata);
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        if (progress === 0) {
+        }
+        if (progress === 100) {
+        }
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          db.collection("users").doc(uid).update({ photo: downloadURL });
+          dispatch(batchUpdate(downloadURL, uid));
+          dispatch({ type: LOADING, payload: false });
+        });
+      }
+    );
+  }
+};
 
 export const batchUpdate = (downloadURL, uid) => async (dispatch, getState) => {
-	try {
-		const batch = db.batch();
-		const posts = await db.collection('posts').get()
+  try {
+    const batch = db.batch();
+    const posts = await db.collection("posts").get();
+    const comments = await db.collection("comments").get();
 
-		let array = []
-		posts.forEach((post) => {
-			if (post.data().uid === uid) {
-				array.push(post.data())
-			}
-		})
+    let array = [];
+    posts.forEach((post) => {
+      if (post.data().uid === uid) {
+        array.push(post.data());
+      }
+    });
 
-		array.map(doc => {
-			const post = db.doc(`/posts/${doc.id}`);
-			batch.update(post, { photo: downloadURL });
-		})
+    let array2 = [];
+    comments.forEach((comment) => {
+      if (comment.data().commenterId === uid) {
+        array2.push(comment.data());
+      }
+    });
 
-		posts.forEach((post, i) => {
-			post.data().comments.map((comment, i) => {
-				const batchComment = db.doc(`/posts/${post.id}/comments[${i}].${comment.commenterId}`)
-				batch.update(batchComment, {commenterPhoto: downloadURL})
-			})
-		})
-		batch.commit();
-		dispatch({ type: UPDATE_PROFILE_PICTURE, payload: downloadURL })
-		dispatch(getPosts())
-		dispatch(getUser(uid, 'PROFILE'))
-	} catch (error) {
-		alert(error)
-	}
-}
+    array2.map((doc) => {
+      const comment = db.doc(`/comments/${doc.id}`);
+      batch.update(comment, { commenterPhoto: downloadURL });
+    });
+
+    array.map((doc) => {
+      const post = db.doc(`/posts/${doc.id}`);
+      batch.update(post, { photo: downloadURL });
+    });
+
+    batch.commit();
+    dispatch({ type: UPDATE_PROFILE_PICTURE, payload: downloadURL });
+    dispatch(getPosts());
+    dispatch(getUser(uid, "PROFILE"));
+  } catch (error) {
+    alert(error);
+  }
+};
 
 export const login = () => async (dispatch, getState) => {
-	Keyboard.dismiss();
-	dispatch({ type: LOADING, payload: true });
-	dispatch({ type: SET_ERROR, payload: null })
-	const { email, password, token } = getState().user;
-	if (email !== undefined && password !== undefined) {
-		firebase.auth().signInWithEmailAndPassword(email, password).then(response => {
-			dispatch({ type: LOADING, payload: false });
-			dispatch({ type: SET_ERROR, payload: null })
-			dispatch(getUser(response.user.uid));
-		}).catch(error => {
-			dispatch({ type: LOADING, payload: false })
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			dispatch({ type: SET_ERROR, payload: errorMessage })
-		})
-	} else {
-		dispatch({ type: LOADING, payload: false });
-		dispatch({ type: SET_ERROR, payload: "Fill all required fields" })
-	}
-}
+  Keyboard.dismiss();
+  dispatch({ type: LOADING, payload: true });
+  dispatch({ type: SET_ERROR, payload: null });
+  const { email, password, token } = getState().user;
+  if (email !== undefined && password !== undefined) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        dispatch({ type: LOADING, payload: false });
+        dispatch({ type: SET_ERROR, payload: null });
+        dispatch(getUser(response.user.uid));
+      })
+      .catch((error) => {
+        dispatch({ type: LOADING, payload: false });
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        dispatch({ type: SET_ERROR, payload: errorMessage });
+      });
+  } else {
+    dispatch({ type: LOADING, payload: false });
+    dispatch({ type: SET_ERROR, payload: "Fill all required fields" });
+  }
+};
 
 export const getUser = (uid, type) => async (dispatch, getState) => {
-	const { token } = getState().user
-	try {
-		db.collection('users').doc(uid).onSnapshot((querySnapshot) => {
-			if (token != null) {
-				db.collection('users').doc(uid).update({ token: token })
-			}
-			if (type === SIGN_IN) {
-				dispatch({ type: SIGN_IN, payload: querySnapshot.data() })
-			}
+  const { token } = getState().user;
+  try {
+    db.collection("users")
+      .doc(uid)
+      .onSnapshot((querySnapshot) => {
+        if (token != null) {
+          db.collection("users").doc(uid).update({ token: token });
+        }
+        if (type === SIGN_IN) {
+          dispatch({ type: SIGN_IN, payload: querySnapshot.data() });
+        }
 
-			dispatch({ type: GET_PROFILE, payload: querySnapshot.data() })
-		})
-	} catch (error) {
-		console.log(error)
-	}
-}
+        dispatch({ type: GET_PROFILE, payload: querySnapshot.data() });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const signOut = () => dispatch => {
-	firebase.auth().signOut();
-	dispatch({ type: SIGN_OUT })
-	dispatch({ type: UPDATE_BIO, payload: '' })
-	dispatch({ type: UPDATE_USERNAME, payload: '' })
-	dispatch({ type: UPDATE_EMAIL, payload: '' })
-	dispatch({ type: UPDATE_PASSWORD, payload: '' })
-}
+export const signOut = () => (dispatch) => {
+  firebase.auth().signOut();
+  dispatch({ type: SIGN_OUT });
+  dispatch({ type: UPDATE_BIO, payload: "" });
+  dispatch({ type: UPDATE_USERNAME, payload: "" });
+  dispatch({ type: UPDATE_EMAIL, payload: "" });
+  dispatch({ type: UPDATE_PASSWORD, payload: "" });
+};
 
 export const getAllUsers = () => async (dispatch, getState) => {
-	try {
-		const users = await db.collection('users').get();
-		let array = [];
-		users.forEach((user) => {
-			array.push(user.data())
-		})
-		dispatch({ type: GET_ALL_USERS, payload: array })
-	} catch (error) {
-		alert(error)
-	}
-}
+  try {
+    const users = await db.collection("users").get();
+    let array = [];
+    users.forEach((user) => {
+      array.push(user.data());
+    });
+    dispatch({ type: GET_ALL_USERS, payload: array });
+  } catch (error) {
+    alert(error);
+  }
+};
 
 export const signup = () => (dispatch, getState) => {
-	Keyboard.dismiss();
-	const hex_md5v = md5.hex_md5(Date.now() + "");
-	dispatch({ type: LOADING, payload: true })
-	const { email, password, username, bio, token } = getState().user;
-	if (email === undefined || password === undefined || username === undefined || bio === undefined) {
-		dispatch({ type: LOADING, payload: false })
-		dispatch({ type: SET_ERROR, payload: "Fill all required fields" })
-	}
-	else if (email !== undefined && password !== undefined) {
-		dispatch({ type: SET_ERROR, payload: null })
-		firebase.auth().createUserWithEmailAndPassword(email, password).then(response => {
-			const user = {
-				uid: response.user.uid,
-				email: email,
-				username: username,
-				bio: bio,
-				photo: `http://gravatar.com/avatar/${hex_md5v}?d=identicon`,
-				token: token ? token : null,
-				followers: [],
-				following: [],
-				createdAt: new Date().getTime(),
-			}
-			db.collection('users').doc(response.user.uid).set(user)
-			dispatch({ type: LOADING, payload: false })
-			dispatch({ type: SIGN_IN, payload: user })
-		}).catch(error => {
-			dispatch({ type: LOADING, payload: false })
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			dispatch({ type: SET_ERROR, payload: errorMessage })
-		})
-	}
-
-}
+  Keyboard.dismiss();
+  const hex_md5v = md5.hex_md5(Date.now() + "");
+  dispatch({ type: LOADING, payload: true });
+  const { email, password, username, bio, token } = getState().user;
+  if (
+    email === undefined ||
+    password === undefined ||
+    username === undefined ||
+    bio === undefined
+  ) {
+    dispatch({ type: LOADING, payload: false });
+    dispatch({ type: SET_ERROR, payload: "Fill all required fields" });
+  } else if (email !== undefined && password !== undefined) {
+    dispatch({ type: SET_ERROR, payload: null });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        const user = {
+          uid: response.user.uid,
+          email: email,
+          username: username,
+          bio: bio,
+          photo: `http://gravatar.com/avatar/${hex_md5v}?d=identicon`,
+          token: token ? token : null,
+          followers: [],
+          following: [],
+          createdAt: new Date().getTime(),
+        };
+        db.collection("users").doc(response.user.uid).set(user);
+        dispatch({ type: LOADING, payload: false });
+        dispatch({ type: SIGN_IN, payload: user });
+      })
+      .catch((error) => {
+        dispatch({ type: LOADING, payload: false });
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        dispatch({ type: SET_ERROR, payload: errorMessage });
+      });
+  }
+};
 
 export const updateUserDetails = (navigate) => async (dispatch, getState) => {
-	dispatch({ type: LOADING, payload: true })
-	const { username, bio, uid } = getState().user;
+  dispatch({ type: LOADING, payload: true });
+  const { username, bio, uid } = getState().user;
 
-	db.collection('users').doc(uid).update({ username: username, bio: bio });
-	const batch = db.batch();
-	const posts = await db.collection('posts').get();
+  db.collection("users").doc(uid).update({ username: username, bio: bio });
+  const batch = db.batch();
+  const posts = await db.collection("posts").get();
 
-	let array = []
-	posts.forEach((post) => {
-		if (post.data().uid === uid) {
-			array.push(post.data())
-		}
-	});
+  let array = [];
+  posts.forEach((post) => {
+    if (post.data().uid === uid) {
+      array.push(post.data());
+    }
+  });
 
-	array.map(doc => {
-		const post = db.doc(`/posts/${doc.id}`);
-		batch.update(post, { username: username });
-	});
-	batch.commit();
-	navigate.navigate('MyProfile')
-	dispatch(getUser(uid));
-	dispatch(getPosts());
-	dispatch({ type: LOADING, payload: false })
-}
+  array.map((doc) => {
+    const post = db.doc(`/posts/${doc.id}`);
+    batch.update(post, { username: username });
+  });
+  batch.commit();
+  navigate.navigate("MyProfile");
+  dispatch(getUser(uid));
+  dispatch(getPosts());
+  dispatch({ type: LOADING, payload: false });
+};
 
 export const followUser = (user) => async (dispatch, getState) => {
-	const { uid, photo, username } = getState().user
-	try {
-		db.collection('users').doc(user.uid).update({
-			followers: firebase.firestore.FieldValue.arrayUnion(uid)
-		})
-		db.collection('users').doc(uid).update({
-			following: firebase.firestore.FieldValue.arrayUnion(user.uid)
-		})
-		db.collection('notifications').doc().set({
-			followerId: uid,
-			followerPhoto: photo,
-			followerName: username,
-			uid: user.uid,
-			photo: user.photo,
-			username: user.username,
-			createdAt: new Date().getTime(),
-			type: 'FOLLOWER',
-		})
-	} catch (e) {
-		console.log(e)
-	}
-}
+  const { uid, photo, username } = getState().user;
+  try {
+    db.collection("users")
+      .doc(user.uid)
+      .update({
+        followers: firebase.firestore.FieldValue.arrayUnion(uid),
+      });
+    db.collection("users")
+      .doc(uid)
+      .update({
+        following: firebase.firestore.FieldValue.arrayUnion(user.uid),
+      });
+    db.collection("notifications").doc().set({
+      followerId: uid,
+      followerPhoto: photo,
+      followerName: username,
+      uid: user.uid,
+      photo: user.photo,
+      username: user.username,
+      createdAt: new Date().getTime(),
+      type: "FOLLOWER",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const unfollowUser = (user) => async (dispatch, getState) => {
-	const { uid, photo, username } = getState().user
-	try {
-		db.collection('users').doc(user.uid).update({
-			followers: firebase.firestore.FieldValue.arrayRemove(uid)
-		})
-		db.collection('users').doc(uid).update({
-			following: firebase.firestore.FieldValue.arrayRemove(user.uid)
-		})
-	} catch (e) {
-		console.log(e)
-	}
-}
+  const { uid, photo, username } = getState().user;
+  try {
+    db.collection("users")
+      .doc(user.uid)
+      .update({
+        followers: firebase.firestore.FieldValue.arrayRemove(uid),
+      });
+    db.collection("users")
+      .doc(uid)
+      .update({
+        following: firebase.firestore.FieldValue.arrayRemove(user.uid),
+      });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 // export const updateUser = () => async (dispatch, getState) => {
 // 	const { uid, username, photo, bio } = getState().user;
